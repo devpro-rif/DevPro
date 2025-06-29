3f8c8b77b9495406ff600a97182aece1ab362260
Ademsalah <adem.hameed.123@gmail.com>
2025-06-30 00:09:35 +0100

    payment done back and front

---
+++ client/src/pages/PaymentPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import StripeContainer from '../components/payment/StripeContainer';
+
+const PaymentPage = () => (
+  <main >
+  
+    <StripeContainer />
+  </main>
+);
+
+export default PaymentPage;
