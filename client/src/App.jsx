1d39e8a0571971b4ddafef1c00a2e6518b739849
AmineBenHadjMessaoud <aminebenhadjmessaoud@gmail.com>
2025-07-01 18:48:18 +0200

    private routes and not found page commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import PaymentPage from './pages/PaymentPage';
 import CampaignPage from './pages/CampaignPage';
 import CampaignDetailPage from './pages/CampaignDetailPage';
@@ -8,19 +9,78 @@ import Login from './components/login/Login';
 import Register from './components/login/Register';
 import EditUser from './components/userProfile/EditUser';
 import NavBar from './components/NavBar';
+import PrivateRoute from './components/PrivateRoute'; 
+import NotFound from './components/NotFound';
 
 const App = () => (
   <Router>
     <NavBar />
     <Routes>
-      <Route path="/campaigns" element={<CampaignPage />} />
-      <Route path="/campaigns/:id" element={<CampaignDetailPage />} />
-      <Route path="/campaigns/:id/contribute" element={<ContributionForm />} />
-      <Route path="/payment" element={<PaymentPage />} />
+      {/* Public routes */}
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/user/edit" element={<EditUser />} />
+
+      {/* Protected routes */}
+      <Route
+         path="/" 
+         element={
+          <PrivateRoute>
+            <HomePage />
+          </PrivateRoute>
+         
+         } />
+      <Route
+        path="/campaigns"
+        element={
+          <PrivateRoute>
+            <CampaignPage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/campaigns/:id"
+        element={
+          <PrivateRoute>
+            <CampaignDetailPage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/campaigns/:id/contribute"
+        element={
+          <PrivateRoute>
+            <ContributionForm />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/payment"
+        element={
+          <PrivateRoute>
+            <PaymentPage />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/user/edit"
+        element={
+          <PrivateRoute>
+            <EditUser />
+          </PrivateRoute>
+        }
+      />
+      
+        
+        
+       
+        
+        
+       
+        {/*catch unfound routes*/}
+    <Route path="*" element={<NotFound />} />
     </Routes>
+
+  
   </Router>
 );
 
