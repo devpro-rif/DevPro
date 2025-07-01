2e48634618bad505da39712a240678ddd3f6049f
AmineBenHadjMessaoud <aminebenhadjmessaoud@gmail.com>
2025-07-01 15:05:22 +0200

    edit user profile commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import PaymentPage from './pages/PaymentPage';
 import CampaignPage from './pages/CampaignPage';
 import Login from './components/login/Login';
 import Register from './components/login/Register';
+import EditUser from './components/userProfile/EditUser';
 
 const App = () => (
   <Router>
@@ -12,6 +13,7 @@ const App = () => (
       <Route path="/payment" element={<PaymentPage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/user/edit" element={<EditUser />} />
     </Routes>
   </Router>
 );
